0944dcf09eac56211b08ebdad99b425901b3ede1
横田昴輝 <kokiyokotakoki@gmail.com>
2024-11-25 23:18:47 +0900

    ui 調整

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,36 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
+import { Card, CardContent,CardTitle,CardHeader } from "@/components/ui/card";
+
+const supabase = createServerComponentClient({ cookies});
+
+const getAllLessons = async () => {
+  const {data:lessons} = await supabase.from("lesson").select("*");
+  return lessons;
+}
 
 export default async function Home() {
-  const supabase = createServerComponentClient({ cookies});
-  const {data:lessons} = await supabase.from("lesson").select("*");
+  const lessons = await getAllLessons();  
 
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {lessons?.map(lesson => {
-          return(
-            <Link href={`/${lesson.id}`} key={lesson.id}>
-              {lesson.title}
-            </Link>
-          )
-        })}
+      <main className="w-full max-w-3xl mx-auto my-16 px-2">
+        <div className="flex flex-col gap-3">
+          {lessons?.map(lesson => {
+            return(
+              <Link href={`/${lesson.id}`} key={lesson.id}>
+                <Card>
+                  <CardHeader>
+                    <CardTitle>{lesson.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <p>{lesson.description}</p>
+                  </CardContent>
+                </Card>
+              </Link>
+            )
+          })}
+        </div>
       </main>
   );
 }
